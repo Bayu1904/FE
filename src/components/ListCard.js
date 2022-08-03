4ec67fd6849ff480ab8cb9ae5f6cf241145f77bd
Torao-Law <dandisyahrullah.12@gmail.com>
2022-08-03 13:58:26 +0700

    add-link

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
@@ -15,16 +15,16 @@ export default function ListCard() {
       <Row>
         {DataBoba.map((items, index) => (
           <Col className="mt-4">
-            <Card key={index} style={{ width: "15.063rem", border: 0, borderRadius: 10, backgroundColor: "#F6DADA" }}
-            >
-              {/* <Link to="/detail-product"> */}
-              <Card.Img variant="top" src={items.image} className="rounded" style={{ height: "19.5rem" }} />
-              <Card.Body>
-                <p className="fw-bold text-danger" style={{ fontSize: "18px", margin: 0 }}>{items.title}</p>
-                <p style={{ color: "#974A4A", margin: 0 }}>{formatPrice(items.price)}</p>
-              </Card.Body>
-              {/* </Link> */}
-            </Card>
+            <Link to={`/detail-project/${index}`}>
+              <Card key={index} style={{ width: "15.063rem", border: 0, borderRadius: 10, backgroundColor: "#F6DADA" }}
+              >
+                <Card.Img variant="top" src={items.image} className="rounded" style={{ height: "19.5rem" }} />
+                <Card.Body>
+                  <p className="fw-bold text-danger" style={{ fontSize: "18px", margin: 0 }}>{items.title}</p>
+                  <p style={{ color: "#974A4A", margin: 0 }}>{formatPrice(items.price)}</p>
+                </Card.Body>
+              </Card>
+            </Link>
           </Col>
         ))}
       </Row>
