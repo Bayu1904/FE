9fbb1c9e723154f0ba6cb7c9d4deadab49f790ea
Torao-Law <dandisyahrullah.12@gmail.com>
2022-08-05 11:28:55 +0700

    create-cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OrderPages from "./pages/orderPages";
 import LandingPage from "./pages/Landingpages";
 import Login from "./components/inputForm/Login";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/product-detail/:id" element={<OrderPages />} />
+          <Route path="/myOrder" element={<Cart />} />
         </Routes>
       </Router>
     </div>
